6439f1be1256c0f49e976010f45a65318f01b66c
Susan Meerdink <susan.meerdink@gmail.com>
2016-05-31 10:08:20 -0700

    File to add .dat to file names

---
+++ Miscellaneous/rename_files_with_dat.py
@@ -0,0 +1,25 @@
+##Renaming Files
+#This script runs through a directory and renames MASTER files to end in .dat.
+#Susan Meerdink
+#5//16
+#--------------------------------------------------------------
+
+import os # os is a library that gives us the ability to make OS changes
+import glob
+ 
+directory = 'H:\\users\\meerdink\\Image_To_Image_Registration\\' #Set directory
+fl_list = ['1','2','3','4','5','6','7','8','9','10','11'] #Set the flightlines you want to rename
+
+for folder in fl_list: #Loop through folders
+    os.chdir(directory + 'SB_FL' + folder + '\\AVIRIS\\') #Change directory to the current folder
+    files_list = glob.glob('*') #Get list of all files in directory
+    print('Renaming files in folder: ' + 'FL' + folder)
+
+    for one in files_list: #Loop through files
+        if '.hdr' not in one: #If it is a header file DON'T Change name
+            if '.dat' not in one: #If it hasn't already been renamed
+                new = one + '.dat' #New File name
+                os.rename(one, new) #Rename the file
+            
+    
+print('Done Renaming Files')
